ac2d428cf63a08d242b0e454fa4f9717a65f52e1
uberBuilder24 <70218688+uberBuilder24@users.noreply.github.com>
2020-11-27 16:24:09 -0500

    Add files via upload

--- Dr. Mario/DrMario.py
+++ Dr. Mario/DrMario.py
@@ -1,1 +1,25 @@
-# There is no code yet... We must finish the Candy Crush Test first.
+import pygame
+
+# Initialize Pygame
+pygame.init()
+
+# Create the Screen
+screen = pygame.display.set_mode((800, 600))
+
+# Background
+background = pygame.image.load('Background.png')
+
+# Title and Icon
+pygame.display.set_caption('Dr. Mario')
+icon = pygame.image.load('Logo.png')
+pygame.display.set_icon(icon)
+
+# Game Loop
+running = True
+while running:
+    screen.blit(background, (0, 0))
+    # Check for Events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()  # Updates Screen
